4fa3b1a24882d8cf8ca046735adeaad63d344cda
Renan Rodrigues <renanssba@gmail.com>
2017-11-01 00:18:38 -0300

    improvements to conquest code

--- conquest-code.js
+++ conquest-code.js
@@ -4,10 +4,13 @@
 
 handlers.sendHighscore = function(args, context){
   
-  if(args && args.challengedPlayerId && args.stageId && args.score){
-    log.debug("Valid parameters. Continue execution");
-  }else{
+  if(args == null || args.challengedPlayerId == null ||
+     args.stageId == null || args.score == null || args.displayName == null){
     return {error: "INVALID_PARAMETERS"};
+  }
+  
+  if(context == null || context.playerProfile == null || context.playerProfile.DisplayName == null){
+    return {error: "CONTEXT_UNAVAILABLE_IN_THIS_CALL"};
   }
   
   
@@ -15,22 +18,38 @@ handlers.sendHighscore = function(args, context){
   var stageName = "stage".concat( args.stageId.toString() );
   var submittedScore = args.score;
   
-  var request = {
-    PlayFabId: currentPlayerId
-  };
-  var getUserDataResult = server.GetUserData(request);
-  
-  if( getUserDataResult.Data[stageName] == null ){
-    return {error: "STAGE_NOT_INITIALIZED"};
-  } 
+  var getUserDataResult = server.GetUserData({ PlayFabId: challengedPlayerId });
+  if(getUserDataResult.Data[stageName] == null){ // no stage data found for this id
+    
+    /// TODO: initialize the data when I own the region
+    if(currentPlayerId != challengedPlayerId){
+      
+      var stageData = {
+        [stageName]: stageName,
+        highscore: submittedScore,
+        ownerId: currentPlayerId,
+        ownerDisplayName: "DISPLAY NAME"
+      }
+      
+      var request = {
+        Data: stageData,
+        Permission: Public
+      }
+      
+      requestResult = server.UpdateUserData(request);
+      return {result: "SUCCESS"}
+      
+    }else{
+      return {error: "STAGE_NOT_INITIALIZED"};
+    }
+  }
   
   var currentHighscore = getUserDataResult.Data[stageName].Value.highscore;
   
-  
   var response = {
-    regionName: getUserDataResult.Data[stageName].Value,
+    regionName: getUserDataResult.Data.regionName.Value,
     highscore: currentHighscore
-  }  
+  };
   return{message: response};
 }
 
