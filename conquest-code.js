f61f2530b7271ae0273e5af9fd575f4e61c1b564
Renan Rodrigues <renanssba@gmail.com>
2017-11-01 02:23:13 -0300

    adding updateStageData function and small improvement to getting stage highscore

--- conquest-code.js
+++ conquest-code.js
@@ -9,6 +9,7 @@ handlers.sendHighscore = function(args, context){
     return {error: "INVALID_PARAMETERS"};
   }
   
+  
   var challengedPlayerId = args.challengedPlayerId;
   var stageName = "stage";
   stageName = stageName.concat(args.stageId.toString());
@@ -17,36 +18,58 @@ handlers.sendHighscore = function(args, context){
   var getUserDataResult = server.GetUserData({ PlayFabId: challengedPlayerId });
   if(getUserDataResult.Data[stageName] == null){ // no stage data found for this id
     
-    /// TODO: initialize the data when I own the region
     if(currentPlayerId == challengedPlayerId){
+      // initialize this stage data
       
-      var stageData = {
-        highscore: submittedScore,
-        ownerId: currentPlayerId,
-        ownerDisplayName: args.displayName
-      };
-      var requestData = {};
-      requestData[stageName] = JSON.stringify(stageData);
-      
-      var request = {
-        PlayFabId: challengedPlayerId,
-        Data: requestData,
-        Permission: "Public"
-      }
-      var requestResult = server.UpdateUserData(request);
-      return {result: "SUCCESS"}
+      return handlers.updateStageData( {
+        playerId: currentPlayerId,
+        stageId: args.stageId,
+        score: submittedScore,
+        displayName: args.displayName
+      }, context);
       
     }else{
+      // cant initialize this stage data
       return {error: "STAGE_NOT_INITIALIZED"};
     }
   }
   
-  var currentHighscore = getUserDataResult.Data[stageName].Value.highscore;
+  var currentHighscore = JSON.parse(getUserDataResult.Data[stageName].Value).highscore;
   
   var response = {
     //regionName: getUserDataResult.Data.regionName.Value,
     highscore: currentHighscore
   };
-  return{message: response};
+  return {message: response};
+}
+
+
+
+handlers.updateStageData = function(args, context){
+  
+  if(args == null || args.playerId == null ||
+     args.stageId == null || args.score == null || args.displayName == null){
+    return {error: "INVALID_PARAMETERS"};
+  }
+  
+  
+  var stageName = "stage";
+  stageName = stageName.concat(args.stageId.toString());
+  
+  var stageData = {
+    highscore: args.score,
+    ownerId: args.playerId,
+    ownerDisplayName: args.displayName
+  };
+  var requestData = {};
+  requestData[stageName] = JSON.stringify(stageData);
+  
+  var request = {
+    PlayFabId: challengedPlayerId,
+    Data: requestData,
+    Permission: "Public"
+  }
+  var requestResult = server.UpdateUserData(request);
+  return {result: requestResult.status}
 }
 
