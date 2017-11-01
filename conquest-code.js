00f39308d50866cf67b1d10d9f38a9ece24da850
Renan Rodrigues <renanssba@gmail.com>
2017-11-01 00:25:48 -0300

    working on conquest

--- conquest-code.js
+++ conquest-code.js
@@ -7,12 +7,7 @@ handlers.sendHighscore = function(args, context){
   if(args == null || args.challengedPlayerId == null ||
      args.stageId == null || args.score == null || args.displayName == null){
     return {error: "INVALID_PARAMETERS"};
-  }
-  
-  if(context == null || context.playerProfile == null || context.playerProfile.DisplayName == null){
-    return {error: "CONTEXT_UNAVAILABLE_IN_THIS_CALL"};
-  }
-  
+  }  
   
   var challengedPlayerId = args.challengedPlayerId;
   var stageName = "stage".concat( args.stageId.toString() );
@@ -28,7 +23,7 @@ handlers.sendHighscore = function(args, context){
         [stageName]: stageName,
         highscore: submittedScore,
         ownerId: currentPlayerId,
-        ownerDisplayName: "DISPLAY NAME"
+        ownerDisplayName: args.displayName
       }
       
       var request = {
